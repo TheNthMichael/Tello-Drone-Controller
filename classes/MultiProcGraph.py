ebab71669e517a99c93cc376a7e4f1a094e65d05
Michael R Nickerson <45344096+MirynW@users.noreply.github.com>
2021-05-01 14:49:01 -0400

    changes

--- classes/MultiProcGraph.py
+++ classes/MultiProcGraph.py
@@ -16,6 +16,15 @@ def startGraph(q, title):
 class DroneGraph:
     def __init__(self, q, title):
         super().__init__()
+        self.previousEstimatedPoses = {
+            "x": [0],
+            "y": [0],
+            "z": [0],
+            "u": [0],
+            "v": [0],
+            "w": [0],
+            "t": [0],
+        }
         self.q = q
         self.title = title
         self.figure = plt.figure()
@@ -27,18 +36,26 @@ class DroneGraph:
     def RXDataLoop(self):
         while True:
             try:
-                previousEstimatedPoses = q.get(True, 2.0)  # Wait a couple of seconds
+                previousEstimatedPoses = self.q.get(True, 1.0)  # Wait a couple of seconds
             except queue.Empty:
                 print("Data Not Received...\nClosing Grapher...")
-                sys.exit()
-
+                #sys.exit()
+            self.previousEstimatedPoses["x"].append(previousEstimatedPoses["x"][-1])
+            self.previousEstimatedPoses["y"].append(previousEstimatedPoses["y"][-1])
+            self.previousEstimatedPoses["z"].append(previousEstimatedPoses["z"][-1])
+            self.previousEstimatedPoses["u"].append(previousEstimatedPoses["u"][-1])
+            self.previousEstimatedPoses["v"].append(previousEstimatedPoses["v"][-1])
+            self.previousEstimatedPoses["w"].append(previousEstimatedPoses["w"][-1])
             self.ax.quiver(
-                previousEstimatedPoses["x"],
-                previousEstimatedPoses["y"],
-                previousEstimatedPoses["z"],
-                previousEstimatedPoses["u"],
-                previousEstimatedPoses["v"],
-                previousEstimatedPoses["w"],
+                self.previousEstimatedPoses["x"],
+                self.previousEstimatedPoses["y"],
+                self.previousEstimatedPoses["z"],
+                self.previousEstimatedPoses["u"],
+                self.previousEstimatedPoses["v"],
+                self.previousEstimatedPoses["w"],
+                length=10,
+                normalize=True,
                 color="b",
             )
             self.figure.canvas.draw()
+            plt.pause(1/1000)
